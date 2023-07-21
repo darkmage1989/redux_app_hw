18b796791b60d581c35710a84820c59a7f65a962
darkmage1989 <darkmage1989@gmail.com>
2023-07-22 02:33:41 +0300

    домашка

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 function App() {
+  const ref = useRef()
+  const [todoText, setTodoText] = useState("");
+  const dispatch = useDispatch();
+  const toDos = useSelector((state) => state.toDo);
+  const id = useSelector((state) => state.id);
+  const addTodo = () => {
+    ref.current.value = ''
+    if (!todoText) {
+      return
+    }
+    dispatch({
+      type: "ADD_TODO",
+      payload: { id: id + 1, content: todoText, isComplete: false },
+    });
+    setTodoText('')
+  };
+  const todoCatch = (event) => {
+    const target = event.target;
+    setTodoText(target.value);
+  };
+  const toogleTodo = (event) => {
+    const id = event.target.id;
+    dispatch({
+      type: "TOOGLE_TODO",
+      payload: id,
+    });
+  };
+  const deleteTodo = (event) => {
+    const id = event.target.dataset.id;
+    dispatch({
+      type: "DELETE_TODO",
+      payload: id,
+    });
+  };
+
+  const notComplete = toDos.filter((todo) => !todo.isComplete);
+  const Complete = toDos.filter((todo) => todo.isComplete);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div style={{ display: "flex" }}>
+        <input ref={ref} onInput={todoCatch}></input>
+        <button onClick={addTodo}>добавить</button>
+      </div>
+      <div style={{ display: "flex" }}>
+        <ul>
+          {notComplete.map((toDo) => (
+            <li
+              id={toDo.id}
+              onClick={toogleTodo}
+              className = {`todo ${toDo.isComplete ? "isComlpete" : "isNoComlete"}`}
+              key={toDo.id}
+            >
+              {toDo.content}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {Complete.map((toDo) => (
+            <>
+              <li
+                id={toDo.id}
+                onClick={toogleTodo}
+                className={toDo.isComplete ? "isComlpete" : "isNoComlete"}
+                key={toDo.id}
+              >
+                {toDo.content}
+              </li>
+              <button className="btn" data-id={toDo.id} onClick={deleteTodo}>
+                Удалить
+              </button>
+            </>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+  id: 0,
+  toDo: [],
+};
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        id: state.id + 1,
+        toDo: [...state.toDo, action.payload],
+      }
+      case "TOOGLE_TODO": 
+      const id = action.payload
+      const item = state.toDo.find((e) => e.id === +id)
+      const setComplete = {...item, isComplete: !item.isComplete}
+      const items = state.toDo.map((e) => e.id===+id? setComplete: e)
+      return{...state, toDo: items
+      }
+      case "DELETE_TODO":
+        console.log(action.payload);
+        return{...state, toDo: state.toDo.filter((el) => el.id !==+action.payload)
+        }
+    default:
+      return state;
+  }
+};
+
+export const store = configureStore({ reducer });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
